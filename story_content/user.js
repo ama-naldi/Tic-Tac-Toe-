febc1e7af6eb2f100fc21ca1763ba55803bd45cf
ama-naldi <ronaldiguns19@gmail.com>
2025-05-15 14:11:00 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,19 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5d3Pbq4TzMd":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = dokument.getElementByld('bgSongku');
+audio.src='musik8bit.mp3";
+audio.load0;
+audio.play0;
+audio.volume=0,3;
+}
+
